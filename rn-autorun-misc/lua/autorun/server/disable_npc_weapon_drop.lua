23fb1a1cc7a2ce8bb5cce327324cd9c82a443727
sirhChristoph <43757048+sirhChristoph@users.noreply.github.com>
2024-08-27 22:07:21 +0100

    Delete rn-autorun-misc/lua/autorun directory

--- rn-autorun-misc/lua/autorun/server/disable_npc_weapon_drop.lua
+++
@@ -1,7 +0,0 @@
-hook.Add("OnNPCKilled", "DisableNPCWeaponDrop", function(npc)
-    if IsValid(npc) then
-        for _, weapon in ipairs(npc:GetWeapons()) do
-            weapon:Remove()
-        end
-    end
-end)
