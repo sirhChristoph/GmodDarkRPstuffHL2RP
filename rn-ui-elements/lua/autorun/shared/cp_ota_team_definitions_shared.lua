edbb6e5fbe2bfa30085a33eec52cc22c2edeb2d7
sirhChristoph <43757048+sirhChristoph@users.noreply.github.com>
2024-08-27 21:45:21 +0100

    Update cp_ota_team_definitions_shared.lua

--- rn-ui-elements/lua/autorun/shared/cp_ota_team_definitions_shared.lua
+++ rn-ui-elements/lua/autorun/shared/cp_ota_team_definitions_shared.lua
@@ -17,7 +17,7 @@ hook.Add("loadCustomDarkRPItems", "DefineTeams", function() -- Waits for DarkRP 
         [TEAM_OTAOR] = true,
     }
 
-    _G.allowedTeams = {
+    _G.allowedTeams = { -- globally accessible
         [TEAM_CPR] = true,
         [TEAM_CPO] = true,
         [TEAM_CPTL] = true,
