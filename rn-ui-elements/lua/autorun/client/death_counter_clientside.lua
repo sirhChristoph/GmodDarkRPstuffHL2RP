5abea77afb674586f7efded369d6b528f6465947
sirhChristoph <43757048+sirhChristoph@users.noreply.github.com>
2024-08-27 21:42:11 +0100

    Update death_counter_clientside.lua

--- rn-ui-elements/lua/autorun/client/death_counter_clientside.lua
+++ rn-ui-elements/lua/autorun/client/death_counter_clientside.lua
@@ -14,8 +14,8 @@ hook.Add("HUDPaint", "DisplayDeathCounters", function()
     local ply = LocalPlayer()
     if CivilProtectionTeams and OTATeams then
         if CivilProtectionTeams[ply:Team()] or OTATeams[ply:Team()] then
-            draw.SimpleText("Civil Protection BSLs: " .. CivilProtectionDeaths, "DebugOverlay", 40, 160, Color(255, 255, 255, 255), TEXT_ALIGN_LEFT, TEXT_ALIGN_TOP)
-            draw.SimpleText("OTA BSLs: " .. OTADeaths, "DebugOverlay", 40, 180, Color(255, 255, 255, 255), TEXT_ALIGN_LEFT, TEXT_ALIGN_TOP)
+            draw.SimpleText("Civil Protection BSLs: " .. CivilProtectionDeaths, "DebugOverlay", 40, 160, Color(255, 255, 255, 255), TEXT_ALIGN_LEFT, TEXT_ALIGN_TOP) -- "Debug Overlay" is a default font with gmod, X and Y values for position follow.
+            draw.SimpleText("OTA BSLs: " .. OTADeaths, "DebugOverlay", 40, 180, Color(255, 255, 255, 255), TEXT_ALIGN_LEFT, TEXT_ALIGN_TOP) -- Same here, adds additional line for OTA Deaths defined in serverside file.
         end
     end
 end)
