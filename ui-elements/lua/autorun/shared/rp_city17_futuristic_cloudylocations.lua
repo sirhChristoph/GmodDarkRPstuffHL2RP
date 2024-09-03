4170037a465a0617681be3f420652a2ba55a0f4b
sirhChristoph <43757048+sirhChristoph@users.noreply.github.com>
2024-09-03 16:29:42 +0100

    Add files via upload

---
+++ ui-elements/lua/autorun/shared/rp_city17_futuristic_cloudylocations.lua
@@ -0,0 +1,16 @@
+--autorun/shared/rp_city17_futuristic_cloudylocations.lua
+rp_city17_futuristic_cloudylocations = {
+    {min = Vector(6081.744629, 4946.155762, -907.063965), max = Vector(9685.734375, 173.016052, 8341.149414), message = "Location: Central Nexus"},
+    {min = Vector(4849.627930, 3235.451660, -36.790047), max = Vector(5946.839844, 3963.225098, 361.033478), message = "Location: Nexus Courtyard"},
+    {min = Vector(5129.583984, 2689.142090, 19.110104), max = Vector(4607.874512, 3225.889404, 251.685211), message = "Location: Ration Distribution"},
+    {min = Vector(2712.600342, 2770.290771, 23.587631), max = Vector(-672.090881, 6064.377930, 481.173523), message = "Location: Train Station"},
+    {min = Vector(5812.299805, 5352.444336, 53.495323), max = Vector(1468.692139, 9587.726563, 799.975159), message = "Location: District 3"},
+    {min = Vector(4418.666992, 5292.683105, 18.125544), max = Vector(2717.296631, 1767.769287, 628.656982), message = "Location: District 7"},
+    {min = Vector(4142.349121, 1731.757324, 23.199673), max = Vector(35.147045, -2353.857422, 584.996216), message = "Location: District 2"},
+    {min = Vector(-273.576843, -2331.443115, -48.475281), max = Vector(-5988.291016, 1633.874390, 736.977051), message = "Location: District 4"},
+    {min = Vector(-5491.469727, 3540.781738, -489.588623), max = Vector(-14604.594727, 10217.945313, 826.109375), message = "Outlands Zone 1"},
+    {min = Vector(6824.229004, 8819.703125, -526.429321), max = Vector(14706.496094, 14801.827148, 921.621521), message = "Outlands Zone 2"},
+    -- Add other zones below with a comma at the end of the zone definition. {min = Vector(0, 0, 0), max = Vector(1, 1, 1), message = "Location: Example"},
+}
+
+return rp_city17_futuristic_cloudylocations
